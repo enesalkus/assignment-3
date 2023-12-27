96153bc2624051d67a13a923393b3a3ef9eb4e6e
Enes <enesalkus@hotmail.com>
2023-12-27 05:32:32 +0100

    Upload

---
+++ assignment-3/assignment-3/Program.cs
@@ -0,0 +1,20 @@
+﻿
+int max = int.Parse(Console.ReadLine());
+
+int[] numbers = new int[max + 1];
+
+for (int i = 2; i < numbers.Length; i++) numbers[i] = i;
+
+for (int i = 2; i < Math.Sqrt(numbers.Length - 1) + 1; i++)
+{
+    if (numbers[i] != 0)
+    {
+        for (int j = (int)Math.Pow(numbers[i], 2); j < numbers.Length; j += numbers[i])
+            numbers[j] = 0;
+    }
+}
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] != 0) Console.WriteLine(numbers[i]);
+}
